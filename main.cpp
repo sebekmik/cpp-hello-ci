25b1a8f762b4f407deae1e41b30c599df03150be
sebekmik <61986004+sebekmik@users.noreply.github.com>
2026-01-02 01:31:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello from CI!" << std::endl;
+  std::cout << "Hello from GitHub CI!" << std::endl;
   std::cout << "2 + 2 = " << (2 + 2) << std::endl;
   std::cout << "CI is running my program." << std::endl;
   return 0;
